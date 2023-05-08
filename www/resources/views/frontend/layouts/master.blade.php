ac250107317c2ddd5ed82c01f5801aa0b5721a90
Dev2BinDev <dev2.binarydevelopers@gmail.com>
2023-05-08 15:14:11 +0530

    Meta title & description change

--- www/resources/views/frontend/layouts/master.blade.php
+++ www/resources/views/frontend/layouts/master.blade.php
@@ -2,10 +2,11 @@
 <html lang="en">
 
 <head>
-  <title>KS Metals Mining</title>
   <meta charset="UTF-8" >
   <meta http-equiv="X-UA-Compatible" content="IE=edge" >
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+  <title>Largest Self-Owned Mine in the Country</title>
+  <meta name="description" content="A business under the KS Cheema Group of Companies, KS Metals, spread over 77 acres of land, is a significant contributor to the metal and mining industry in India.">
   <meta name="csrf-token" content="{{ csrf_token() }}"/>
   <link rel="apple-touch-icon" sizes="180x180" href="{{asset('assets/frontend/favicon_io/apple-touch-icon.png')}}">
   <link rel="icon" type="image/png" sizes="32x32" href="{{asset('assets/frontend/favicon_io/favicon-32x32.png')}}/">
